c11393484bd774f0f6c46182e46d459849849e7e
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-11-18 22:52:11 +1100

    Setting up board manager

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+	[Serializable]
+	public class Count {
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max) {
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int colums = 8;
+	public int rows = 8;
+	public Count wallCount = new Count(5, 9);
+	public Count foodCount = new Count(1, 5);
+	public GameObject exit;
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+	public GameObject[] foodTiles;
+	public GameObject[] enemyTiles;
+	public GameObject[] outterWallTiles;
+	
+	public Transform boardHolder;
+	public List<Vector3> gridPositions = new List<Vector3>();
+
+	void InitializeList() {
+		gridPositions.Clear();
+
+		for (int x = 1; x < colums - 1; x++) {
+			for (int y = 1; y < rows - 1; y++) {
+				gridPositions.Add(new Vector3(x, y, 0f));
+			}
+		}
+	}
+
+	private void BoardSetup() {
+		boardHolder = new GameObject("Board").transform;
+
+		for(int x = -1; x < colums + 1; x++) {
+			for(int y = -1; x < rows + 1; y++) {
+				GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+				if (x == -1 || x == colums || y == -1 || y == rows) {
+					toInstantiate = outterWallTiles[Random.Range(0, outterWallTiles.Length)];
+				}
+
+				GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+				
+				instance.transform.SetParent(boardHolder);
+			}
+		}
+	}
+
+	private Vector3 RandomPosition() {
+		int randomIndex = Random.Range(0, gridPositions.Count);
+		Vector3 randomPosition = gridPositions[randomIndex];
+		
+		// Make sure it's not a duplicate
+		gridPositions.RemoveAt(randomIndex);
+		
+		return randomPosition;
+	}
+
+	private void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum) {
+		int objectCount = Random.Range(minimum, maximum + 1);
+
+		for (int i = 0; i < objectCount; i++) {
+			Vector3 randomPosition = RandomPosition();
+			GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+			Instantiate(tileChoice, randomPosition, Quaternion.identity);
+		}
+	}
+
+	public void SetupScene(int level) {
+		BoardSetup();
+		InitializeList();
+		LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
+		LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
+		int enemyCount = (int)Mathf.Log(level, 2f);
+		LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+		Instantiate(exit, new Vector3(colums - 1, rows - 1, 0f), Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+public BoardManager boardScript;
+private int level = 3;
+
+	void Awake () {
+		boardScript = GetComponent<BoardManager>();
+		InitGame();
+	}
+
+	void InitGame() {
+		
+	}
+}
