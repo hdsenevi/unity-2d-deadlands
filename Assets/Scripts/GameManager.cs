63f2dff33383a480df5f02249532a2eb71112954
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-11-21 08:31:59 +1100

    Added a leader script to manage loading game manager and game board

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+	public static GameManager instance = null;
 
-public BoardManager boardScript;
-private int level = 3;
+	public BoardManager boardScript;
+	private int level = 3;
 
 	void Awake () {
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
 		boardScript = GetComponent<BoardManager>();
 		InitGame();
 	}
@@ -15,4 +23,5 @@ private int level = 3;
 	void InitGame() {
 		boardScript.SetupScene(level);
 	}
+
 }
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+	public GameManager gameManager;
+	void Awake () {
+		if (GameManager.instance == null) {
+			Instantiate(gameManager);
+		}		
+	}
+}
