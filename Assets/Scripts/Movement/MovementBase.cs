111ffdccd9901973d8c16c7519a9428a563da156
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-11-30 08:29:59 +1100

    Updated prefabs

---
+++ Assets/Scripts/Movement/MovementBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovementBase : MonoBehaviour {
+	public float moveTime = 0.1f;
+	public LayerMask blockingLayer;
+
+	private BoxCollider2D boxCollider;
+	private Rigidbody2D rb2D;
+	private float inverseMoveTime;
+
+	protected virtual void Start() {
+		boxCollider = GetComponent<BoxCollider2D>();
+		rb2D = GetComponent<Rigidbody2D>();
+		inverseMoveTime = 1f / moveTime;
+	}
+}
