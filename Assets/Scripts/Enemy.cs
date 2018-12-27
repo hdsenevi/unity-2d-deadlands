684df500334d5878b48023d2aab4b677be140e33
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-12-28 00:10:28 +1100

    Enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MovementBase
+{
+    public int playerDamage;
+
+    private Animator animator;
+    private Transform target;
+    private bool skipMove;
+
+    protected override void Start()
+    {
+        animator = GetComponent<Animator>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        base.Start();
+    }
+
+    protected override void AttemptMove<T>(int xDir, int yDir)
+    {
+        if (skipMove)
+        {
+            skipMove = false;
+        }
+
+        base.AttemptMove<T>(xDir, yDir);
+
+        skipMove = true;
+    }
+
+    public void MoveEnemy()
+    {
+        int xDir = 0;
+        int yDir = 0;
+
+        if (Mathf.Abs(target.position.x - transform.position.x) < float.Epsilon)
+        {
+            yDir = target.position.y > transform.position.y ? 1 : -1;
+        }
+        else
+        {
+            xDir = target.position.x > target.position.x ? 1 : -1;
+        }
+
+        AttemptMove<Player>(xDir, yDir);
+    }
+
+    protected override void OnCantMove<T>(T component)
+    {
+        Player hitPlayer = component as Player;
+
+        hitPlayer.LoseFood(playerDamage);
+    }
+}
