e1abefa55deeb1e61cdc1d29c1678acfe4a41870
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-12-27 20:05:50 +1100

    Uncommited changes

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    public Sprite dmgSprite;
+    public int hp = 4;
+
+    void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
