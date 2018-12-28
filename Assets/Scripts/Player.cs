ac5be5cd4bfe6bcb920f2b38ec667266ae292caf
Shanaka 'SHa' Senevirathne <shanaka36@gmail.com>
2018-12-28 20:29:32 +1100

    Touch controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,6 +20,7 @@ public class Player : MovementBase
 
     private Animator animator;
     private int food;
+    private Vector2 touchOrigin = -Vector2.one;
 
     protected override void Start()
     {
@@ -37,6 +38,7 @@ public class Player : MovementBase
         int horizontal = 0;
         int vertical = 0;
 
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
         horizontal = (int)Input.GetAxisRaw("Horizontal");
         vertical = (int)Input.GetAxisRaw("Vertical");
 
@@ -44,6 +46,34 @@ public class Player : MovementBase
         {
             vertical = 0;
         }
+#else
+        if (Input.touchCount > 0)
+        {
+            Touch myTouch = Input.touches[0];
+
+            if (myTouch.phase == TouchPhase.Began)
+            {
+                touchOrigin = myTouch.position;
+            }
+            else if (myTouch.phase == TouchPhase.Ended && touchOrigin.x >= 0)
+            {
+                Vector2 touchEnd = myTouch.position;
+                float x = touchEnd.x - touchOrigin.x;
+                float y = touchEnd.y - touchOrigin.y;
+                touchOrigin.x = -1;
+
+                // Guess which direction in general use is swiping to 
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    horizontal = x > 0 ? 1 : -1;
+                }
+                else
+                {
+                    vertical = y > 0 ? 1 : -1;
+                }
+            }
+        }
+#endif
 
         if (horizontal != 0 || vertical != 0)
         {
